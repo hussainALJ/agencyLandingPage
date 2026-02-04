3f705dbfca4a0fb91d4305b3eb313b7042f7ad7e
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2026-02-04 16:41:17 +0300

    Make the basic styles and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
+import Home from './pages/Home.jsx'
 
 function App() {
 
   return (
     <>
+    <Home />
     </>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header.jsx"
+
+function Home () {
+    return (
+        <>
+        <Header />
+        </>
+    )
+}
+
+export default Home
